7e5cf125e9fe521bd34ae6f155b74cbeb7fd0f86
xzjh <jsxzjh@gmail.com>
2014-05-11 19:39:17 +0800

    init

---
+++ Scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="false" />
+/// <reference path="jquery-1.10.2.js" />
+/// <reference path="jquery-migrate-1.2.1.js" />
+/// <reference path="jquery-ui-1.10.3.js" />
+/// <reference path="modernizr-2.6.2.js" />
